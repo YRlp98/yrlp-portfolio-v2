5221f9959cf59f74c82161e70f595eed1e009859
Yousef <yousef.r1999@gmail.com>
2026-08-22 00:56:08 +0330

    fix: improve TextOnlyButton hover effect

--- components/widgets/TextOnlyButton.vue
+++ components/widgets/TextOnlyButton.vue
@@ -22,17 +22,21 @@ export default {
 
 <style lang="scss" scoped>
 .textOnlyButton {
+  position: relative;
+  z-index: 2;
+  display: inline-block;
   text-decoration: none;
   color: white;
   background-color: var(--gray-3);
   padding: 16px 32px;
   padding-bottom: 14px; // To fix font padding issue
   border-radius: 5px;
+  transition: color 0.5s ease-in, transform 0.5s ease-in, box-shadow 0.5s ease-in;
 
   &:hover {
+    color: var(--green-1);
     transform: scale(1.1);
-    transition: 0.5s ease-in;
-    box-shadow: 0px 4px 90px 1px rgba(255, 255, 255, 0.1);
+    box-shadow: 0 4px 90px 1px rgba(53, 233, 37, 0.25);
   }
 }
 </style>
